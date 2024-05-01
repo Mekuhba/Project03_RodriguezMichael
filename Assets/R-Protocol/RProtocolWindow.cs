c0d8396bccfed0f141fd1756582cb7774555d9d4
Michael Rodriguez <Mar190011@utdallas.edu>
2024-04-30 22:42:56 -0500

    added script to the tool, tool FUNCTIONS
    the tool now works in my ideal version, if more components of different types need to be added, then variations of lines 92-117 must be added. apparently, different types need to be specified in order for the script to access different types of components from folders in the root folder fields.

--- Assets/R-Protocol/RProtocolWindow.cs
+++ Assets/R-Protocol/RProtocolWindow.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class RProtocolWindow : EditorWindow
 {
     private GameObject targetObject;
-    public DefaultAsset folderObject;
+    public DefaultAsset[] folderObjects;
 
     // Add a menu item named "Custom Tool" to the Window menu
     [MenuItem("Window/R-Protocol Tool")]
@@ -29,28 +29,94 @@ public class RProtocolWindow : EditorWindow
         // Draw the "Randomize" button
         if (GUILayout.Button("Randomize"))
         {
-            // Add your randomization logic here
-            Debug.Log("Randomize button clicked.");
+            RandomizeSelections();
         }
 
         // Draw the "Clear Selections" button
         if (GUILayout.Button("Clear"))
         {
-            // Add your logic to clear selections here
-            Debug.Log("Clear Selections button clicked.");
+            ClearFolders();
         }
 
-        // Draw the "Clear Selections" button
+        // Draw the "Add Root" button
         if (GUILayout.Button("Add Root"))
         {
-            // Add your logic to clear selections here
-            Debug.Log("Clear Selections button clicked.");
+            AddRootFolder();
         }
 
         GUILayout.EndHorizontal();
         ///////////// End the horizontal layout /////////////////////
 
-        // Draw the first root field for dragging and dropping a target folder
-        folderObject = (DefaultAsset)EditorGUILayout.ObjectField("Root Folder", folderObject, typeof(DefaultAsset), true);
+        // Draw the root folder fields
+        for (int i = 0; i < folderObjects.Length; i++)
+        {
+            folderObjects[i] = (DefaultAsset)EditorGUILayout.ObjectField("Root Folder", folderObjects[i], typeof(DefaultAsset), true);
+        }
+    }
+
+    private void ClearFolders()
+    {
+        // Clear all root folder fields except the first one
+        if (folderObjects.Length > 1)
+        {
+            folderObjects = new DefaultAsset[] { folderObjects[0] };
+        }
+    }
+
+    private void AddRootFolder()
+    {
+        // Add a new root folder field
+        DefaultAsset[] newArray = new DefaultAsset[folderObjects.Length + 1];
+        folderObjects.CopyTo(newArray, 0);
+        folderObjects = newArray;
+    }
+
+    private void RandomizeSelections()
+    {
+        // Clear existing components on the target object
+        foreach (var component in targetObject.GetComponents<MonoBehaviour>())
+        {
+            DestroyImmediate(component);
+        }
+
+        // Iterate over each folder object
+        foreach (var folderObject in folderObjects)
+        {
+            // Get the path of the folder
+            string folderPath = AssetDatabase.GetAssetPath(folderObject);
+
+            // Find all script files within the folder
+            string[] scriptGUIDs = AssetDatabase.FindAssets("t:MonoScript", new[] { folderPath });
+
+            // If there are script files in the folder, select one randomly and add it as a component
+            if (scriptGUIDs.Length > 0)
+            {
+                string randomScriptGUID = scriptGUIDs[Random.Range(0, scriptGUIDs.Length)];
+                string randomScriptPath = AssetDatabase.GUIDToAssetPath(randomScriptGUID);
+                var randomScript = AssetDatabase.LoadAssetAtPath<MonoScript>(randomScriptPath);
+                targetObject.AddComponent(randomScript.GetClass());
+            }
+
+            // Find all material files within the folder
+            string[] materialGUIDs = AssetDatabase.FindAssets("t:Material", new[] { folderPath });
+
+            // If there are material files in the folder, select one randomly and apply it to the target object
+            if (materialGUIDs.Length > 0)
+            {
+                string randomMaterialGUID = materialGUIDs[Random.Range(0, materialGUIDs.Length)];
+                string randomMaterialPath = AssetDatabase.GUIDToAssetPath(randomMaterialGUID);
+                var randomMaterial = AssetDatabase.LoadAssetAtPath<Material>(randomMaterialPath);
+                var renderer = targetObject.GetComponent<Renderer>();
+
+                if (renderer != null)
+                {
+                    renderer.sharedMaterial = randomMaterial;
+                }
+                else
+                {
+                    Debug.LogWarning("Target object does not have a Renderer component.");
+                }
+            }
+        }
     }
 }
